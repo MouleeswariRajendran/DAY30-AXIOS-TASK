cdf24c60d083570afd0bd053d97cafdd3ea998b7
MouleeswariRajendran <145764370+MouleeswariRajendran@users.noreply.github.com>
2023-12-27 17:29:47 +0530

    Add files via upload

---
+++ src/Store.jsx
@@ -0,0 +1,11 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+
+import { UserSlice } from "./Redux/redux";
+
+
+export const store = configureStore({
+ reducer:{
+    app : UserSlice.reducer
+ }
+})
